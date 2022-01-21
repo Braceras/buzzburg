b84adcc60ab45b3384e9d631a361d5936f880a51
Benja Braceras <benjabracerasm@gmail.com>
2022-01-20 23:09:08 -0300

    commit js 17

--- js/pedidos.js
+++ js/pedidos.js
@@ -3,14 +3,18 @@
 
 $(()=> {
     mostrarContenido();
-    obtenerProductos();
+    
+    obtenerHams();
     imprimirCarrito(carrito);
+    
     
 });
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-let productos;
+let hamburguesas = JSON.parse(localStorage.getItem("hamburguesas")) || [];
+
+let hamburs;
 
 
 
@@ -37,27 +41,21 @@ function mostrarContenido(){
     
     
 }
-/*
-    const titulo = $("<h3></h3>");
-    titulo.html("Realiza tu pedido en Buzzburg");
-    titulo.hide();
-    const nodoTitutlo = $(".mainClass");
-    nodoTitutlo.prepend(titulo);
-    titulo.slideDown(1000); */
 
-function obtenerProductos(){
-    $.get("../data/productos.json", (respuesta, estado) => {
-        productos = respuesta.products
-        imprimirProductos(productos);
+
+function obtenerHams(){
+    
+    $.get("../data/hamburs.json", (respuesta, estado) => {
+        ham = respuesta.hams;
+        imprimirHams(ham);
     });
 }
 
 
 
-
-function imprimirProductos(array){
+function imprimirHams(array){
     
-    
+    $("#productos").empty();
     array.forEach((prod) => {
         $("#productos").append(
             `   <div class="burbuja">
@@ -76,7 +74,7 @@ function imprimirProductos(array){
 function agregarAlCarrito(e){
     
     let id = Number(e.target.id);
-    let productoElegido = productos.find((p) => p.id === id);
+    let productoElegido = ham.find((p) => p.id === id);
     
     carrito.push(productoElegido);
 
@@ -120,267 +118,4 @@ function eliminarProducto(e){
 
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function init(){
-    listarCategorias();
-}
-
-function listarCategorias(){
-
-    const nodoCategorias= $("#categorias")[0];
-
-    let contenido="<ul>";
-
-    for(categoria of categorias)
-    {
-        contenido+=`<li onclick="listarProductos(${categoria.id})">${categoria.nombre}</li>`;
-        
-    }
-
-    contenido+="</ul>";
-
-    nodoCategorias.innerHTML=contenido;
-}
-
-
-
-
-
-function listarProductos(idCategoria)
-{
-
-    const nodoProductos=$("#productos")[0];
-    
-    let lista = "<div>";
-
-    const productosCategoria=productos.filter(elemento => elemento.categorias===idCategoria)
-
-    for(producto of productosCategoria)
-    {
-        
-        lista+=`<div class="burbuja">
-                <img src = ${producto.img} />
-                <h4>${producto.nombre}</h4>
-                <p>${producto.orden}</p>
-                <p>Precio: $${producto.precio}</p>
-                <button>Agregar a mi pedido</button></div><hr>
-                ${actualizarCarrito()}`;
-        }
-
-        lista+= "</div>";
-
-    nodoProductos.innerHTML=lista;
-
-       
-}
-
-
-
-function actualizarCarrito()
-{
-    const nodoCarrito = $("#carrito"); 
-
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function agregarPedidoHam1()
-{
-    const buzzburger = new hamburguesa("Buzzburger", "cheddar", "lechuga, jitomate", "BuzzSauce", false, 500) 
-
-    console.log(buzzburger);
-    alert("SU ORDEN ES: "+buzzburger.nombre+ " $"+buzzburger.precio);
-
-    JSON.stringify(buzzburger);
-
-    const nodoCarrito = document.getElementById("carrito");
-    nodoCarrito.innerHTML="";
-    let producto = document.createElement("div");
-    producto.innerHTML=`<hr><div><h3>${buzzburger.nombre}</h3></div>
-                        <div>$${buzzburger.precio}</div>`;
-
-    nodoCarrito.appendChild(producto);
-
-                        
-
-    
-
-}
-function agregarPedidoHam2() 
-{
-    const cheeseburger = new hamburguesa("Cheeseburger", "cheddar", false, "BuzzSauce", false, 400)
-
-    console.log(cheeseburger);
-    alert("SU ORDEN ES: "+cheeseburger.nombre+ " $"+cheeseburger.precio);
-
-    JSON.stringify(cheeseburger);
-    
-}
-function agregarPedidoHam3()
-{
-    const chiliBuzzburger = new hamburguesa("Chili Buzzburger", "cheddar", false, "Mostaza Suave", ["chile picante, pickles"], 600)
-
-    console.log(chiliBuzzburger);
-    alert("SU ORDEN ES: "+chiliBuzzburger.nombre+ " $"+chiliBuzzburger.precio);
-
-    JSON.stringify(chiliBuzzburger);
-    
-}
-function agregarPedidoHam4()
-{
-    const butterBuzzburger = new hamburguesa("Butter Buzzburger", "cheddar", false, false, ["Cebolla reahogada en manteca y fondo de carne, manteca"], 600)
-
-    console.log(butterBuzzburger);
-    alert("SU ORDEN ES: "+butterBuzzburger.nombre+ " $"+butterBuzzburger.precio);
-
-    JSON.stringify(butterBuzzburger);
-    
-}
-function agregarPedidoHam5()
-{
-    const doubleBuzzburger = new hamburguesa("Double-Double Buzzburger", "cheddar", "lechuga, tomate", "BuzzSauce", "lonja de cebolla", 600)
-
-    console.log(doubleBuzzburger);
-    alert("SU ORDEN ES: "+doubleBuzzburger.nombre+ " $"+doubleBuzzburger.precio);
-
-    JSON.stringify(doubleBuzzburger);
-    
-}
-function agregarPedidoHam6()
-{
-    const smokeBuzz = new hamburguesa("Smoke Buzz", "cheddar", "ají", "BuzzSauce", "bacon", 600)
-
-    console.log(smokeBuzz);
-    alert("SU ORDEN ES: "+smokeBuzz.nombre+ " $"+smokeBuzz.precio);
-
-    JSON.stringify(smokeBuzz);
-    
-} 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
